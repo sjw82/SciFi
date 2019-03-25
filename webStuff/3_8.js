b9ad423360cf51e62a46969d94b2e22206849b80
sjw82 <samanthawatkins98@gmail.com>
2019-03-25 11:39:38 -0400

    add highlighting

--- webStuff/3_8.js
+++ webStuff/3_8.js
@@ -1,15 +1,32 @@
-window.addEventListener("DOMContentLoaded", init, false)
+window.addEventListener("DOMContentLoaded", init, false);
 function init() {
-    const buttons = document.querySelectorAll(".button")
+    const buttons = document.querySelectorAll(".button");
     for (var i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener("click", toggle, false)
+        buttons[i].addEventListener("click", toggle, false);
+    }
+    const checkboxes = document.querySelectorAll("input[type='checkbox']");
+    for (var i = 0; i < checkboxes.length; i++) {
+        checkboxes[i].addEventListener("click", highlight, false);
     }
 }
 function toggle() {
-    const panels = document.querySelectorAll("#wrapper > div")
-    for (var i = 0; i < panels.length; i++){
-        panels[i].style.display = "none"
+    const panels = document.querySelectorAll("#wrapper > div");
+    for (var i = 0; i < panels.length; i++) {
+        panels[i].style.display = "none";
     }
     const selected = this.id + "text";
-    document.querySelector("#" + selected).style.display = "block"
+    document.querySelector("#" + selected).style.display = "block";
+}
+
+function highlight() {
+    var targets = document.getElementsByClassName(this.id);
+    if (this.checked) {
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].classList.add("highlight");
+        }
+    } else {
+        for (var i = 0; i < targets.length; i++) {
+            targets[i].classList.remove("highlight");
+        }
+    }
 }
